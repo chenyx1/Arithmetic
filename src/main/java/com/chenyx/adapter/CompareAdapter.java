d933912cc54970182b15f788654d12c0b6371167
lixiaoxun <18986276735@189.cn>
2019-01-30 17:06:13 +0800

    更新设配器

--- src/main/java/com/chenyx/adapter/CompareAdapter.java
+++ src/main/java/com/chenyx/adapter/CompareAdapter.java
@@ -1,6 +1,7 @@
 package com.chenyx.adapter;
 
 import com.chenyx.compare.Compare;
+import com.chenyx.constant.CompareResult;
 import com.chenyx.factory.CompareFactory;
 import com.chenyx.factory.SortFactory;
 import org.slf4j.Logger;
@@ -22,11 +23,23 @@ public abstract class CompareAdapter<T> {
     }
 
     /**
-     * @desc 比较结果设配方法，
+     * @desc 比较结果设配方法，用于大于或小于比较。具体属于哪一类比较根据比较器类型决定
      * return  :  false 失败
      *           true 成功
      * @author chenyx
      * @date 2018-07-30
      * */
     public abstract boolean compare(T t1,T t2) throws Exception;
+    /***
+     * @desc 检测是否相等
+     * @author chenyx
+     *@date 2019-01-08
+     * */
+    public boolean equal(T t1, T t2) throws Exception {
+        int retult = this.compare.compare(t1 ,t2);
+        if (retult == CompareResult.EQUAL_VALUE.getValue()) {
+            return  true;
+        }
+        return  false;
+    };
 }
